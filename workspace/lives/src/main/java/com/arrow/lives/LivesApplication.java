40feadd20a2c3c1152a99e9e7f6198177536b2eb
hcsilva <henriquec.dasilva@hotmail.com>
2021-02-28 08:41:04 -0300

    Primeiro commit - API pronta

---
+++ workspace/lives/src/main/java/com/arrow/lives/LivesApplication.java
@@ -0,0 +1,13 @@
+package com.arrow.lives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivesApplication.class, args);
+	}
+
+}
---
+++ workspace/lives/src/test/java/com/arrow/lives/LivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrow.lives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
